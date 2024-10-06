6b52ed07b375b6f89de31606fddaca0ad206a233
René El Fatuaki <rene@elfatuaki.com>
2024-10-06 17:09:23 +0200

    Initial commit

---
+++ src/main/kotlin/dev/elfa/inventorymanagement/InventoryManagementApplication.kt
@@ -0,0 +1,11 @@
+package dev.elfa.inventorymanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventoryManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<InventoryManagementApplication>(*args)
+}
