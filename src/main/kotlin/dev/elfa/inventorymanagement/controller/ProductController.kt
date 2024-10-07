6f9fa8ab98b1ec3344654e1af7551abadcee7303
René El Fatuaki <rene@elfatuaki.com>
2024-10-07 19:08:38 +0200

    Implement ProductController with CRUD operations

---
+++ src/main/kotlin/dev/elfa/inventorymanagement/controller/ProductController.kt
@@ -0,0 +1,56 @@
+package dev.elfa.inventorymanagement.controller
+
+import dev.elfa.inventorymanagement.dto.ProductDto
+import dev.elfa.inventorymanagement.model.Product
+import dev.elfa.inventorymanagement.service.ProductService
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PagedModel
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/products")
+class ProductController(private val productService: ProductService) {
+
+    @GetMapping
+    fun getProducts(
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "20") size: Int,
+        @RequestParam(defaultValue = "ASC") direction: Sort.Direction,
+        @RequestParam(defaultValue = "id") sortBy: String
+    ): ResponseEntity<PagedModel<ProductDto>> {
+        val pageable = PageRequest.of(page, size, direction, sortBy)
+        val pageableProducts = productService.getPageableProducts(pageable)
+
+        return ResponseEntity.ok(pageableProducts)
+    }
+
+    @GetMapping("/{id}")
+    fun getProduct(@PathVariable id: String): ResponseEntity<ProductDto> {
+        val productDto = productService.getProduct(id)
+
+        return ResponseEntity.ok(productDto)
+    }
+
+    @PostMapping
+    fun addProduct(@RequestBody product: Product): ResponseEntity<Product> {
+        val createdProduct = productService.addProduct(product)
+
+        return ResponseEntity.ok(createdProduct)
+    }
+
+    @PutMapping("/{id}")
+    fun updateProduct(@PathVariable id: String, @RequestBody product: Product): ResponseEntity<ProductDto> {
+        val productDto = productService.updateProduct(id, product)
+
+        return ResponseEntity.accepted().body(productDto)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteProduct(@PathVariable id: String): ResponseEntity<Unit> {
+        productService.deleteProduct(id)
+
+        return ResponseEntity.noContent().build()
+    }
+}
