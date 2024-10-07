14c3dcfc040840e02c79c0c47d586d9a87e943ab
René El Fatuaki <rene@elfatuaki.com>
2024-10-08 00:02:53 +0200

    Docker Compose Setup for Spring Boot application and database

--- src/main/kotlin/dev/elfa/inventorymanagement/repository/ProductRepository.kt
+++ src/main/kotlin/dev/elfa/inventorymanagement/repository/ProductRepository.kt
@@ -2,5 +2,7 @@ package dev.elfa.inventorymanagement.repository
 
 import dev.elfa.inventorymanagement.model.Product
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ProductRepository : JpaRepository<Product, Long>
