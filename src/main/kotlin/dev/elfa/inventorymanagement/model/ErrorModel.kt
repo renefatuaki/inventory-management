d75c8025ba805dd9aec2284a9a75f639d8eba849
René El Fatuaki <rene@elfatuaki.com>
2024-10-10 18:32:49 +0200

    Renamed error class

--- src/main/kotlin/dev/elfa/inventorymanagement/model/ErrorModel.kt
+++ src/main/kotlin/dev/elfa/inventorymanagement/model/ErrorModel.kt
@@ -2,4 +2,4 @@ package dev.elfa.inventorymanagement.model
 
 data class ErrorMessage(val status: Int, val message: String)
 
-data class ErrorValidation(val field: String, val message: String)
+data class FieldError(val field: String, val message: String)
