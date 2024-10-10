542beefaebb6877fe9b35df4576532764fa9f514
René El Fatuaki <rene@elfatuaki.com>
2024-10-10 19:02:22 +0200

    Added exception handler for runtime exceptions

--- src/main/kotlin/dev/elfa/inventorymanagement/exception/ExceptionControllerAdvice.kt
+++ src/main/kotlin/dev/elfa/inventorymanagement/exception/ExceptionControllerAdvice.kt
@@ -5,12 +5,21 @@ import dev.elfa.inventorymanagement.model.FieldError
 import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class ExceptionControllerAdvice {
+
+    @ExceptionHandler
+    fun handleRuntimeExceptionException(exception: RuntimeException): ResponseEntity<ErrorMessage> {
+        val errorMessage = ErrorMessage(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            exception.message ?: "Internal Server Error"
+        )
+
+        return ResponseEntity(errorMessage, HttpStatus.NOT_FOUND)
+    }
 
     @ExceptionHandler
     fun handleProductNotFoundException(exception: ProductNotFoundException): ResponseEntity<ErrorMessage> {
